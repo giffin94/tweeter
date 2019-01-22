973091ed6fdc1e234021e644d9a11539e69d8de8
Aedan <aedan.giffin@gmail.com>
2019-01-22 21:57:21 +0000

    Refactor character counter script to use ternary operator instead of if/else

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
-  console.log("Ready!");
   $('#newTweet').on('input', function (event) {
     let remainingChars  = 140 - $('#newTweet').val().length;
-    console.log($(this).siblings("span"));
-    $(this).siblings("span").css('color', 'black');
-    if (remainingChars < 0) {
-      $(this).siblings("span").css('color', 'red');
-    } 
+    (remainingChars > 0 ? $(this).siblings("span").css('color', 'black') : $(this).siblings("span").css('color', 'red'));
     $(this).siblings("span")[0].innerText = remainingChars;
   });
 });
-
 
 
