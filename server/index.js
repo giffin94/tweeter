bec59aecb7ac1a7cbfc46dc487e9fa9144b85c84
Aedan <aedan.giffin@gmail.com>
2019-01-22 02:11:45 +0000

    Project start - finish styling Navbar and new tweet form

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 7777;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
@@ -30,5 +30,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("Tweeter listening on port " + PORT);
 });
