d4ba40cc2bfbc11ce66276e2d0458a379b374501
Aedan <aedan.giffin@gmail.com>
2019-01-23 21:48:09 +0000

    Add functional AJAX handlers for get and post to /tweets. Able to get tweets and dynamically render new tweets as they come in.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,51 +1,4 @@
 
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];  
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -54,7 +7,15 @@
 
 $(function() {
 
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
+  
+  function renderTweets(tweetArray) {
+    $('#tweets-container').empty();
+    for(const tweet of tweetArray) {
+      $('#tweets-container').prepend($(createTweetElement(tweet)));
+    }
+    return $('#tweets-container');
+  }
 
   function createTweetElement(tweetObject) {
     let newTweet = $("<article>")
@@ -72,22 +33,39 @@ $(function() {
         $('<footer>').append(
           $('<span/>', {'text': `${tweetObject["created_at"]}`})
           ).append(
-            $('<i/>', {'class': "fa fa-flag"})
+            $('<i>', {'class': "fa fa-flag"})
           ).append(
-            $('<i/>', {"class": 'fa fa-retweet'})
+            $('<i>', {"class": 'fa fa-retweet'})
           ).append(
-            $('<i/>', {"class": 'fa fa-heart'})
+            $('<i>', {"class": 'fa fa-heart'})
           )
       );
     return newTweet;
   }
 
-  function renderTweets(tweetArray) {
-    for(var tweet of tweetArray) {
-      $('#tweets-container').prepend($(createTweetElement(tweet)));
-    }
-    return;
+  var $posts = $('#newtweet');
+  $posts.on('submit', function (event) {
+    event.preventDefault();
+    console.log('Submitted, performing ajax call...');
+    const serialized = $(this).serialize();
+    console.log(serialized);
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: serialized,
+    }).done(loadTweets());
+  });
+
+  function loadTweets() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(function (tweets) {
+      $('#tweets-container').replaceWith(renderTweets(tweets));
+    })
   }
+
+  loadTweets();
+
+
   
 
 });
