918cd9f6c273bb87d1e71d58d000f13dda17e408
Aedan <aedan.giffin@gmail.com>
2019-01-23 07:03:55 +0000

    Add functions to render and get tweets and clean out index.html

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
+$((function() {
   $('#newTweet').on('input', function (event) {
     let remainingChars  = 140 - $('#newTweet').val().length;
-    (remainingChars > 0 ? $(this).siblings("span").css('color', 'black') : $(this).siblings("span").css('color', 'red'));
+    remainingChars > -1 ? $(this).siblings("span").removeClass("invalid") : $(this).siblings("span").addClass("invalid");
+    console.log($(this).siblings("span"));
     $(this).siblings("span")[0].innerText = remainingChars;
   });
-});
+}));
 
 
