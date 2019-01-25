22f3242ea5fa01f4bb212eeed791f06f6556012a
Aedan <aedan.giffin@gmail.com>
2019-01-25 00:34:54 +0000

    Cleanup script - add comment and remove unnecessary code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,13 @@
-$((function() {
+/* Script for handling character counter */
+
+$(function() {
+
   $('#newtweet').children('textarea').on('input', function (event) {
     let remainingChars  = 140 - $(this).val().length;
     remainingChars > -1 ? $(this).siblings("span").removeClass("invalid") : $(this).siblings("span").addClass("invalid");
     $(this).siblings("span").text(remainingChars);
   });
-}));
+
+});
 
 
